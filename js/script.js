5ad9175434a6cc133d6991a2f7d5e55689d6b207
Brendanm14 <128586740+Brendanm14@users.noreply.github.com>
2023-05-10 11:57:54 +0100

    Landingi page created

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$(".button-1").click(function(){
-		$("#screen-1").show(300)
-		$("section").not("#screen-1").hide(300, "linear")
+		$(".screen-1").show(300)
+		$("section").not(".screen-1").hide(300, "linear")
 	})
 	
 	$(".button-2").click(function(){
